816fd4a7bc5518d20faacf5bca46eb97a2f8ff59
GitClubHub <123795596+GitClubHub@users.noreply.github.com>
2025-11-22 21:55:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ logging.basicConfig(
 )
 
 # Токен бота из переменных окружения
-BOT_TOKEN = os.environ['8222449218:AAFgj48oh7Qczvre3l17Tr4FLWmzlWZKVtM']
+BOT_TOKEN = os.environ['BOT_TOKEN']
 
 # Цены за подписку
 PRICES = {
